3c4c5ae77f01759befca0ba341c6d8c47282ba6c
mitchelbreust <mitchell.breust2@gmail.com>
2025-01-01 19:32:03 +1100

    styled walls in mazes and generate random mazes

--- src/components/Maze.jsx
+++ src/components/Maze.jsx
@@ -1,36 +1,57 @@
 import { useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import generateMaze from "../generateMaze";
 import generatePlayerPos from "../generatePlayerPos";
 
 const MazeCont = styled.div`
-    display: grid;
-    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-    width: 800px;
-    height: 800px;
-    border: solid;
-    margin: auto;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  width: 800px;
+  height: 800px;
+  border: solid;
+  margin: auto;
 `;
 
-const PathPiece = styled.div`
-`
+const PathPiece = styled.div``;
 
 const WallPiece = styled.div`
-`
+  background-color: grey;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  border: 5px solid #8b0000;
+  & > div {
+    background-color: #b22222;
+    border: 5px solid #8b0000;
+    position: relative;
+  }
+`;
+
+
 
 const Maze = () => {
-    const [gameMaze, setGameMaze] = useState(generateMaze())
-    const [playerPos, setPlayerPos] = useState(generatePlayerPos());
-    return (
-        <>
-            <MazeCont>
-                {
+  const [gameMaze, setGameMaze] = useState(generateMaze());
+  const [playerPos, setPlayerPos] = useState(generatePlayerPos());
 
-                }
-            </MazeCont>        
-        </>
-    )
-}
+  console.log(gameMaze[0]);
+  return (
+    <>
+      <MazeCont>
+        {gameMaze.map((val, idx) => {
+          if (val === 0) {
+            return (
+              <WallPiece key={idx}>
+                {Array.from({ length: 4 }).map((_, idx) => {
+                  return <div key={idx}></div>;
+                })}
+              </WallPiece>
+            );
+          }
+          return <PathPiece key={idx} />;
+        })}
+      </MazeCont>
+    </>
+  );
+};
 
 export default Maze;
--- src/generateMaze.js
+++ src/generateMaze.js
@@ -1,7 +1,45 @@
 
-
-const generateMaze = () => {
-    return null
-}
-
-export default generateMaze
+const mazes = [
+    // Maze 1
+    [
+      1, 0, 0, 0, 0, 0, 0, 0,
+      1, 1, 1, 0, 1, 1, 1, 0,
+      0, 0, 1, 0, 1, 0, 0, 0,
+      1, 1, 1, 1, 1, 0, 1, 1,
+      0, 0, 0, 0, 1, 0, 1, 0,
+      1, 1, 1, 0, 1, 1, 1, 0,
+      1, 0, 1, 0, 0, 0, 1, 0,
+      1, 0, 1, 1, 1, 1, 1, 1
+    ],
+    // Maze 2
+    [
+      1, 0, 1, 1, 1, 0, 0, 0,
+      1, 0, 1, 0, 1, 0, 1, 1,
+      1, 1, 1, 0, 1, 0, 1, 0,
+      0, 0, 0, 0, 1, 1, 1, 0,
+      1, 1, 1, 1, 1, 0, 0, 0,
+      0, 0, 0, 1, 1, 1, 1, 1,
+      1, 1, 0, 1, 0, 0, 0, 1,
+      0, 1, 0, 1, 1, 1, 0, 1
+    ],
+    // Maze 3
+    [
+      1, 0, 0, 0, 1, 0, 1, 1,
+      1, 1, 1, 0, 1, 0, 0, 1,
+      0, 0, 1, 0, 1, 1, 1, 1,
+      1, 1, 1, 1, 0, 0, 0, 0,
+      0, 0, 0, 1, 1, 1, 1, 1,
+      1, 1, 0, 0, 0, 1, 0, 1,
+      1, 1, 1, 1, 1, 1, 0, 1,
+      0, 0, 0, 0, 0, 1, 0, 1
+    ]
+  ];
+  
+  const generateMaze = () => {
+    // Randomly select one of the predefined mazes
+    const randomIndex = Math.floor(Math.random() * mazes.length);
+    return mazes[randomIndex];
+  };
+  
+  export default generateMaze;
+  
